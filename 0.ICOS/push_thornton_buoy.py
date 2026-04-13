d7d0cd3623b9d4ee138c0339e4d0720b32f7b5d4
Willem Boone <willem.boone@vliz.be>
2026-04-13 13:32:44 +0200

    update thornton sensors links comm

--- 0.ICOS/push_thornton_buoy.py
+++ 0.ICOS/push_thornton_buoy.py
@@ -4,7 +4,9 @@ from OceanOpsClient import OceanOpsClient
 client = OceanOpsClient.from_env()
 
 passport = "passport_thornton_buoy.json"
-# client.validate_passport_json(passport)
+status = client.validate_passport_json(passport)
+pprint(status)
+
 
 m = client.post_passport(passport, dry_run=False)
 pprint(m)
